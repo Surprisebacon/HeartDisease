40c5f402ff9f1160a632b4f5ec6d8cc918c58ee4
Surprisebacon <isak.mathisen@hotmail.no>
2023-04-03 20:24:22 +0200

    commit

--- HeartDisease.py
+++ HeartDisease.py
@@ -1,1 +1,15 @@
+# importing necessary packages
+import pandas as pd 
+import numpy as np
+from sklearn.linear_model import LinearRegression
+clf = LinearRegression()
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn import linear_model
 
+# Reading the file, and make it into a DataFrame
+HD = pd.read_csv('HeartDisease.csv')
+hd = pd.DataFrame(HD)
+
+hd.head()
